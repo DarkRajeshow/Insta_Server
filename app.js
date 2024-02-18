c748ec856d3897ed8d698c6034c63ed0e3d5bfb8
Rajesh Adeli <rajannaadeli@gmail.com>
2024-02-18 18:01:18 +0530

    Adding files in connection just for test

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use("/api/uploads", express.static("uploads"));
+app.use("/api/uploads", express.static("connection/uploads"));
 
 
 // API routes
